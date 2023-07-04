4ea009aa4473433e64b3617ce19af83a53284e49
Nogze <hugo.dufor@gmail.com>
2023-07-04 10:26:03 +0200

    fin

--- src/routes/rooms/[id]/+page.svelte
+++ src/routes/rooms/[id]/+page.svelte
@@ -1,49 +1,46 @@
 <script>
-    import Canvas from "./Canvas.svelte";
-    import {onDestroy, onMount} from "svelte";
-    import {initializeSocket, websocket} from "$lib/socket.js";
-    import Chat from "./Chat.svelte";
-    import Players from "./Players.svelte";
+	import Canvas from './Canvas.svelte';
+	import { onDestroy, onMount } from 'svelte';
+	import { initializeSocket, websocket } from '$lib/socket.js';
+	import Chat from './Chat.svelte';
+	import Players from './Players.svelte';
 
-    export let data
-    let socket
-    let room = {
-        drawHistory: []
-    }
+	export let data;
+	let socket;
+	let room = {
+		drawHistory: []
+	};
 
-    const unsubscribe = websocket.subscribe(val => socket = val)
+	const unsubscribe = websocket.subscribe((val) => (socket = val));
 
-    onMount(() => {
-        initializeSocket()
+	onMount(() => {
+		initializeSocket();
 
-        socket.emit({type: "getRoom", payload: data.id}, (response) => {
-            console.log(response)
-            room = response.payload
-        })
+		socket.emit({ type: 'getRoom', payload: data.id, sender: socket.username }, (response) => {
+			room = response.payload;
+		});
 
-        socket.on("broadcast/joinRoom", (data) => {
-            console.log("data", data)
-            room = data.payload
-        })
+		socket.on('broadcast/joinRoom', (data) => {
+			room = data.payload;
+		});
 
-        socket.on("broadcast/leaveRoom", (data) => {
-            // eslint-disable-next-line no-unused-vars
-            room = data.payload
-        })
-    })
+		socket.on('broadcast/leaveRoom', (data) => {
+			// eslint-disable-next-line no-unused-vars
+			room = data.payload;
+		});
+	});
 
-    onDestroy(() => {
-        socket.emit({type: "leave", sender: socket.username})
-        unsubscribe()
-    })
-
+	onDestroy(() => {
+		socket.emit({ type: 'leave', sender: socket.username });
+		unsubscribe();
+	});
 </script>
 
 <div class="flex flex-col items-center gap-10 mt-20">
-    <h1 class="text-4xl text-center">Room {data.id}</h1>
-    <div class="flex gap-4">
-        <Players room={room}/>
-        <Canvas lineWidth={5} strokeStyle="#000" drawHistory={room.drawHistory}></Canvas>
-        <Chat room={room}></Chat>
-    </div>
+	<h1 class="text-4xl text-center">Room {data.id}</h1>
+	<div class="flex gap-4">
+		<Players {room} />
+		<Canvas drawHistory={room.drawHistory} />
+		<Chat {room} />
+	</div>
 </div>
--- src/routes/rooms/[id]/Canvas.svelte
+++ src/routes/rooms/[id]/Canvas.svelte
@@ -1,87 +1,115 @@
 <script>
-    import {initializeSocket, websocket} from "$lib/socket.js";
-    import {onDestroy, onMount} from "svelte";
+	import { initializeSocket, websocket } from '$lib/socket.js';
+	import { onDestroy, onMount } from 'svelte';
 
-    export let strokeStyle
-    export let lineWidth
-    export let drawHistory
+	const strokeStyles = ['#FFFFCC', '#87CEEB', '#98FF98', '#E6E6FA', '#F8B8C4'];
+	const lineWidths = [1, 2, 3, 4, 5];
 
-    let socket
-    let context
-    let canvas
-    let isDrawing = false
-    let start = {x: null, y: null}
-    let drawnCount = 0
+	export let drawHistory;
 
-    const unsubscribe = websocket.subscribe(val => socket = val)
+	let socket;
 
-    $: printLines(drawnCount, drawHistory.length)
+	let strokeStyle = strokeStyles[0];
+	let lineWidth = lineWidths[0];
+	let context;
+	let canvas;
+	let isDrawing = false;
+	let start = { x: null, y: null };
+	let drawnCount = 0;
 
-    onMount(() => {
-        initializeSocket()
-        context = canvas.getContext("2d")
-        context.lineWidth = lineWidth
-        context.strokeStyle = strokeStyle
+	const unsubscribe = websocket.subscribe((val) => (socket = val));
 
-        socket.on("broadcast/draw", (data) => {
-            drawHistory = [...drawHistory, data.payload]
-        })
-    })
+	$: printLines(drawnCount, drawHistory.length);
 
-    onDestroy(() => {
-        unsubscribe()
-    })
+	onMount(() => {
+		initializeSocket();
+		context = canvas.getContext('2d');
+		context.lineWidth = lineWidth;
+		context.strokeStyle = strokeStyle;
 
-    const printLines = (from, to) => {
-        for (let i = from; i < to; i++) {
-            const {x1, y1, x2, y2, strokeStyle, lineWidth} = drawHistory[i]
-            context.beginPath()
-            context.strokeStyle = strokeStyle
-            context.lineWidth = lineWidth
+		socket.on('broadcast/draw', (data) => {
+			drawHistory = [...drawHistory, data.payload];
+		});
+	});
 
-            context.moveTo(x1, y1)
-            context.lineTo(x2, y2)
-            context.closePath()
-            context.stroke()
-        }
-    }
+	onDestroy(() => {
+		unsubscribe();
+	});
 
-    const handleStartDrawing = (e) => {
-        isDrawing = true
-        const {x, y} = canvas.getBoundingClientRect()
-        const drawX = e.clientX - x
-        const drawY = e.clientY - y
+	const printLines = (from, to) => {
+		for (let i = from; i < to; i++) {
+			const { x1, y1, x2, y2, strokeStyle, lineWidth } = drawHistory[i];
+			context.beginPath();
+			context.strokeStyle = strokeStyle;
+			context.lineWidth = lineWidth;
 
-        start = {x: drawX, y: drawY}
-    }
+			context.moveTo(x1, y1);
+			context.lineTo(x2, y2);
+			context.closePath();
+			context.stroke();
+		}
+	};
 
-    const handleStopDrawing = () => {
-        isDrawing = false
-    }
+	const handleStartDrawing = (e) => {
+		isDrawing = true;
+		const { x, y } = canvas.getBoundingClientRect();
+		const drawX = e.clientX - x;
+		const drawY = e.clientY - y;
 
-    const handleDraw = (e) => {
-        if (!isDrawing) return
-        const {x, y} = canvas.getBoundingClientRect()
-        const drawX = e.clientX - x
-        const drawY = e.clientY - y
+		start = { x: drawX, y: drawY };
+	};
 
-        socket.emit({
-            type: "draw",
-            payload: {x1: start.x, y1: start.y, x2: drawX, y2: drawY, strokeStyle, lineWidth},
-            sender: socket.username
-        })
+	const handleStopDrawing = () => {
+		isDrawing = false;
+	};
 
-        start = {x: drawX, y: drawY}
-    }
+	const handleDraw = (e) => {
+		if (!isDrawing) return;
+		const { x, y } = canvas.getBoundingClientRect();
+		const drawX = e.clientX - x;
+		const drawY = e.clientY - y;
 
+		socket.emit({
+			type: 'draw',
+			payload: { x1: start.x, y1: start.y, x2: drawX, y2: drawY, strokeStyle, lineWidth },
+			sender: socket.username
+		});
+
+		start = { x: drawX, y: drawY };
+	};
 </script>
 
-<canvas
-        bind:this={canvas}
-        on:mousedown={handleStartDrawing}
-        on:mouseup={handleStopDrawing}
-        on:mousemove={handleDraw}
-        width={500}
-        height={500}
-        class="bg-white max-w-[500px] max-h-[500px]"
-></canvas>
+<div class="flex flex-col">
+	<canvas
+		bind:this={canvas}
+		on:mousedown={handleStartDrawing}
+		on:mouseup={handleStopDrawing}
+		on:mousemove={handleDraw}
+		width={500}
+		height={500}
+		class="bg-white max-w-[500px] max-h-[500px]"
+	/>
+	<div
+		class="flex items-center justify-between w-full h-10 gap-4 px-4 bg-gray-500 border-t border-gray-200"
+	>
+		<div class="flex gap-2">
+			{#each strokeStyles as ss}
+				<button
+					on:click={() => (strokeStyle = ss)}
+					style={`background: ${ss};`}
+					class="w-6 h-6 rounded-full"
+				/>
+			{/each}
+		</div>
+		<div class="flex gap-2">
+			{#each lineWidths as lw}
+				<button
+					on:click={() => (lineWidth = lw)}
+					class="flex items-center justify-center w-6 h-6 bg-white border-gray-200 rounded-full"
+				>
+					<div style={`width: ${lw * 2}px; height: ${lw * 2}px;`} class="bg-black rounded-full" />
+				</button>
+			{/each}
+		</div>
+	</div>
+</div>
